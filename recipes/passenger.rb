7ba2e31decf9e0e0e1ab40e2769d4d924edd5cbe
Jason Rankin <rankin.jason@gmail.com>
2013-08-09 17:02:12 -0400

    update passenger recipe to accept any attributes.  Anything attribute added to node["nginx"]["passenger"] except root and gem_binary will now be written out to passenger.conf with the string "passenger_" prepended. e.g. node["nginx"]["passenger"]["buffers"] = "8 16k" would result in a line in passenger.conf like this passenger_buffers 8 16k; where it previously was absent from the list of configurable options. Configuration items with nil values are not written to the configuration file at all, so default values that are overridden to nil can be removed.

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -32,23 +32,14 @@ gem_package 'passenger' do
   gem_binary node["nginx"]["passenger"]["gem_binary"] if node["nginx"]["passenger"]["gem_binary"]
 end
 
+passenger_variables = node["nginx"]["passenger"].reject { |k,v| %w(version gem_binary).include? k }
+
 template "#{node["nginx"]["dir"]}/conf.d/passenger.conf" do
   source "modules/passenger.conf.erb"
   owner "root"
   group "root"
   mode 00644
-  variables(
-    :passenger_root => node["nginx"]["passenger"]["root"],
-    :passenger_ruby => node["nginx"]["passenger"]["ruby"],
-    :passenger_spawn_method => node["nginx"]["passenger"]["spawn_method"],
-    :passenger_use_global_queue => node["nginx"]["passenger"]["use_global_queue"],
-    :passenger_buffer_response => node["nginx"]["passenger"]["buffer_response"],
-    :passenger_max_pool_size => node["nginx"]["passenger"]["max_pool_size"],
-    :passenger_min_instances => node["nginx"]["passenger"]["min_instances"],
-    :passenger_max_instances_per_app => node["nginx"]["passenger"]["max_instances_per_app"],
-    :passenger_pool_idle_time => node["nginx"]["passenger"]["pool_idle_time"],
-    :passenger_max_requests => node["nginx"]["passenger"]["max_requests"]
-  )
+  variables(passenger_variables: passenger_variables)
   notifies :reload, "service[nginx]"
 end
 
